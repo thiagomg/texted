2ca2507613561fb95e1f5edf714bea34f8d7446c
Thiago Massari Guedes <thiedri@gmail.com>
2024-03-04 23:25:13 -0800

    Address show when starting

--- src/bin/texted2/main.rs
+++ src/bin/texted2/main.rs
@@ -6,8 +6,12 @@ use texted2::server::server_run;
 fn open_config() -> Config {
     let exe_path = env::current_exe().unwrap();
     let exe_dir = exe_path.parent().unwrap();
-    println!("cur_dir: {}", env::current_dir().unwrap().to_str().unwrap());
-    read_config(&exe_dir.join("../../texted2.toml")).unwrap()
+    println!("Current dir: {}", env::current_dir().unwrap().to_str().unwrap());
+    let config = read_config(&exe_dir.join("../../texted2.toml")).unwrap();
+
+    println!("Listening on {}:{}", config.server.address, config.server.port);
+
+    config
 }
 
 #[ntex::main]
