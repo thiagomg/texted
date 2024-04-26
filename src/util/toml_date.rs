6a8a50453f7d6b8a387c90a4104b421814d1fa7f
Thiago Massari Guedes <thiedri@gmail.com>
2024-04-25 22:00:25 -0700

    Added unit test

--- src/util/toml_date.rs
+++ src/util/toml_date.rs
@@ -4,7 +4,7 @@ use chrono::{NaiveDate, ParseError};
 use serde::Deserialize;
 
 // Code adapted from https://www.seachess.net/notes/toml-dates/
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct TomlDate(pub NaiveDate);
 
 impl<'de> Deserialize<'de> for TomlDate {
@@ -25,5 +25,34 @@ impl FromStr for TomlDate {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let naive = NaiveDate::from_str(s)?;
         Ok(Self(naive))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde::Deserialize;
+
+    use super::*;
+
+    #[derive(Deserialize)]
+    pub struct Personal {
+        pub activity_start_year: i32,
+        pub blog_start_date: TomlDate,
+    }
+
+    #[derive(Deserialize)]
+    pub struct Config {
+        pub personal: Personal,
+    }
+
+    #[test]
+    fn test_date_time() {
+        let toml_str = r##"
+[personal]
+activity_start_year = 2000
+blog_start_date = 2024-04-22
+"##;
+        let cfg: Config = toml::from_str::<Config>(toml_str).unwrap();
+        assert_eq!(cfg.personal.blog_start_date, TomlDate(NaiveDate::from_ymd_opt(2024, 04, 22).unwrap()));
     }
 }
